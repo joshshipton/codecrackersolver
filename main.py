f57054a2f3c9b8caffe0f5de67d9ad6cc0ab9f91
Josh Shipton <111832259+joshshipton@users.noreply.github.com>
2022-10-16 16:47:32 +0800

    commiting now because i think its gonna break soon

--- main.py
+++ main.py
@@ -12,17 +12,23 @@ unused_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n
                   'w', 'x', 'y', 'z']
 
 # i have two words now need to see possible solutions for both words
-word1 = [15, 9, 15, 15]
-word2 = [15, 9, 15, 14]
-word3 = [9, 7, 14, 13, 14]
-word4 = [13, 20, 9]
+word1 = [1, 10, 8, 16, 4]
+word2 = [1, 2, 2]
+word3 = [2, 7, 13, 17, 2, 1, 3, 16]
+word4 = [8, 14, 13, 12]
+word5 = [3, 4, 5]
+word6 = [18, 1, 4, 16]
 
-board = [word3] + [word2] + [word1] + [word4]
+# if i add word5 and word6 the whole thing breaks but no fucking clue why
+board = [word1] + [word2] + [word3] + [word4]
 
-tplcode = code.items()
+print(board)
 
 # changes the numbers of known words on the board into letters
 #  most popular
+
+# words that have already been tried
+tried = []
 
 counted = []
 counting_list = []
@@ -30,31 +36,31 @@ counting_list = []
 
 
 # fill in the board with known letters
-for i in range(len(board)):
-    for j in range(len(board) - 1):
-        if board[i][j]:
-            a = code.get(board[i][j])
-            if a != '':
-                board[i][j] = a
+# how the fuck did i fuck this up so fucking badly just by adding two words to the board?!?!?
 
+# board is not filling correctly wil have to correct this
+# letters that are filled need to be removed from unused letters
+# need a way to put that back into unsed letters if they are not used
+
+def fillboard(board):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            # if string in board pass
+            if type(board[i][j]) == str:
+                    pass
+            else:
+            # get the value of the key and update the board
+                item = code.get(board[i][j])
+                if item != '':
+                    board[i][j] = item
+    print(board)
+    return board
+
+fillboard(board)
 current = 0
 print(board)
-# counts the occurrences of letters in the first word
 
-for item in board[current]:
-    a = code.get(item)
-    if a != '':
-        pass
-    if a == '' and item not in counted:
-        counts = board[current].count(item)
-        counted.append(item)
-        counting_list.append(counts)
 
-print(counted)
-print(counting_list)
-
-order = list(zip(counting_list, counted))
-print(order)
 
 
 # need to replace every instance of the most common letter with another letter
@@ -66,93 +72,59 @@ check = ''
 # 0 for the letter with the most counts
 # 1 for the second occurance of the tuple, which will be the value tied to the count
 i = 0
-
+run = True
 # copy so i can reference the real b0 as we destroy it
 
-while True:
-    print(iter)
-    iter = board[0].copy()
+while run:
+    board_copy = board[0].copy()
     check = ''
 
     for i in range(len(board[0])):
         if type(board[0][i]) == int:
-            iter[:] = [x if x != iter[i] else unused_letters[cycle] for x in iter]
+            board_copy[:] = [x if x != board_copy[i] else unused_letters[cycle] for x in board_copy]
+            # rand int is not gonna work long term.... but its a solution for now
             cycle = rd.randint(0, len(unused_letters)-1)
 
     # if board[0] has no numbers
     # check if its a word
-        if all(isinstance(item, str) for item in iter):
-            check = check.join(iter)
-            if check in words:
+        if all(isinstance(item, str) for item in board_copy):
+            check = check.join(board_copy)
+            print('h')
+            print(board_copy)
+            if check in words and check not in tried:
+                tried.append(check)
                 print(check + "  yay!")
-                quit()
+                run = False
+                break
+
+
+# update the code-dict
+
+    # get the index of the board[0] numbers and the value of the numbers
+    # the value of the numbers is the key the letter they are now is the value
+# updates the dict with known values
+
+
+
+for item in board[0]:
+    if type(item) == int:
+        find = board[0].index(item)
+        code[item] = board_copy[find]
+
+print(code)
+fillboard(board)
+print(board)
+
+# fill all known words with known knowledge
+    # if any of them are complete check if they are words
+    # if they arent
+    # fill in the next one with a valid word and then check again
 
 
 
 
-# probably best to start with the most common word in the list
-def find_empty():
-    empty_value = {i for i in code if code[i] == ''}
-    empty_value = list(empty_value)
-    if len(empty_value) == 0:
-        return None
-    else:
-        return empty_value[0]
-
-
-# function from the suduko to find if a certain position is valid
-def valid(board, value, pos):
-    pass
-
-
-# need to find all options for one word
-# then try this code for another word
-# if both word try another
-# untill all words are done
-
-def solve():
-    find = find_empty()
-    if not find:
-        return True
-    else:
-        # need to figure out a wah
-        word = find
-
-    # need to fill with random letter instead of random number
-    # also need to add a clause that the dict is not already solved
-    for i in range(1, 26):
-        if valid(code, i, word):
-            word[word][word] = i
-
-        if solve(words):
-            return True
-
-        words[word] = 0
-
-    return False
 
 
 
-# length = int(input("How many letters do you know? "))
+# get word with the least amount of intergers and try that word
 
-
-# print("Enter the number-letter pairs you know")
-# for i in range(length):
-# known_letter = input(f"Enter the {i+1} letter you know: ").lower()
-# known_number = int(input(f"what number does  '{known_letter}' correspond to:  "))
-# code[known_number] = known_letter
-
-# print(code)
-
-
-# user enters known number - letters
-# then enters different combinations of numbers
-# program keeps going until it has cracked the code
-# use a dictionary
-# can just print the dictionary when done
-
-# tries every combination
-# if it words appends it to possible words
-# returns list of possible words
-
-# need to figure out how i can run thru all 26 letters of the alphabet for each int in the list
