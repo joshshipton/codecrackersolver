100f7f89eeb103ca5a4e30c17a77e4d5834a51ee
Josh Shipton <111832259+joshshipton@users.noreply.github.com>
2022-10-16 20:36:04 +0800

    added functions, tried adding version control but not working ;(((((

--- main.py
+++ main.py
@@ -8,123 +8,132 @@ code = {1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: 'l', 8: '', 9: 'p', 10: '',
         16: '', 17: '', 18: '', 19: '', 20: '', 21: '', 22: '', 23: '', 24: '', 25: '', 26: ''}
 
 used_letters = ['l', 'p']
-unused_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'q', 'r', 's', 't', 'u', 'v',
+unused_letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'q', 'r', 's', 't', 'u', 'v',
                   'w', 'x', 'y', 'z']
 
-# i have two words now need to see possible solutions for both words
-word1 = [1, 10, 8, 16, 4]
-word2 = [1, 2, 2]
-word3 = [2, 7, 13, 17, 2, 1, 3, 16]
-word4 = [8, 14, 13, 12]
-word5 = [3, 4, 5]
-word6 = [18, 1, 4, 16]
+word1 = [9, 7, 14, 13, 14]
+word2 = [1, 5, 15, 14, 7]
+word3 = [13, 20, 9]
+word4 = [9, 15, 15, 9]
 
-# if i add word5 and word6 the whole thing breaks but no fucking clue why
 board = [word1] + [word2] + [word3] + [word4]
-
-print(board)
-
-# changes the numbers of known words on the board into letters
-#  most popular
+board_versions = [board]
 
 # words that have already been tried
 tried = []
-
-counted = []
-counting_list = []
-# word we have not yet solved
-
+cycle = rd.randint(0, len(unused_letters) - 1)
+running = True
+version = 0
+index = 0
 
 # fill in the board with known letters
-# how the fuck did i fuck this up so fucking badly just by adding two words to the board?!?!?
 
-# board is not filling correctly wil have to correct this
-# letters that are filled need to be removed from unused letters
-# need a way to put that back into unsed letters if they are not used
-
-def fillboard(board):
+def fill_board():
+    global board
     for i in range(len(board)):
         for j in range(len(board[i])):
             # if string in board pass
             if type(board[i][j]) == str:
-                    pass
+                pass
             else:
-            # get the value of the key and update the board
+                # get the value of the key and update the board
                 item = code.get(board[i][j])
                 if item != '':
                     board[i][j] = item
-    print(board)
+    board_versions.append(board)
     return board
 
-fillboard(board)
-current = 0
-print(board)
+# right now the board is updating so there are no ints in the board, need to revert the changes
+def crackcode():
+    global cycle
+    global running
+    global version
+    global index
+
+    while running:
+        board_copy = board_versions[version][index].copy()
+
+        for i in range(len(board[index])):
+            if type(board[index][i]) == int:
+                board_copy[:] = [x if x != board_copy[i] else unused_letters[cycle] for x in board_copy]
+                cycle = rd.randint(0, len(unused_letters)-1)
 
 
+            if all(isinstance(item, str) for item in board_copy):
+                check = ''
+                check = check.join(board_copy)
+                if check in words and check not in tried:
+                    tried.append(check)
+                    print(check + "  yay!")
+                    # updates the code dictionary
+                    for item in board[index]:
+                        if type(item) == int:
+                            find = board[index].index(item)
+                            code[item] = board_copy[find]
+                            # dont know if i need unused letters, would sure make it a lot faster tho...
+                            # is there a different way to not have duplicates?
+                            #try:
+                                #unused_letters.remove(board_copy[find])
+                            #except ValueError:
+                                #pass
+                            #used_letters.append(board_copy[find])
+                    # updates the code dictionary
+                    running = False
+                    break
+                else:
+                    pass
 
-
-# need to replace every instance of the most common letter with another letter
-# repeat until entire first word is full and a valid word
-c = 0
-cycle = 0
-cycle2 = 0
-check = ''
-# 0 for the letter with the most counts
-# 1 for the second occurance of the tuple, which will be the value tied to the count
-i = 0
-run = True
-# copy so i can reference the real b0 as we destroy it
-
-while run:
-    board_copy = board[0].copy()
-    check = ''
-
-    for i in range(len(board[0])):
-        if type(board[0][i]) == int:
-            board_copy[:] = [x if x != board_copy[i] else unused_letters[cycle] for x in board_copy]
-            # rand int is not gonna work long term.... but its a solution for now
-            cycle = rd.randint(0, len(unused_letters)-1)
-
-    # if board[0] has no numbers
-    # check if its a word
-        if all(isinstance(item, str) for item in board_copy):
-            check = check.join(board_copy)
-            print('h')
-            print(board_copy)
-            if check in words and check not in tried:
-                tried.append(check)
-                print(check + "  yay!")
-                run = False
-                break
-
-
-# update the code-dict
-
-    # get the index of the board[0] numbers and the value of the numbers
-    # the value of the numbers is the key the letter they are now is the value
-# updates the dict with known values
-
-
-
-for item in board[0]:
-    if type(item) == int:
-        find = board[0].index(item)
-        code[item] = board_copy[find]
-
-print(code)
-fillboard(board)
-print(board)
 
 # fill all known words with known knowledge
-    # if any of them are complete check if they are words
-    # if they arent
-    # fill in the next one with a valid word and then check again
+# if any of them are complete check if they are words
+
+
+def check_backtrack():
+    global version
+    version += 1
+    for word in board:
+        if all(isinstance(char, str) for char in word):
+            check = ''
+            check = check.join(word)
+            # getting stuck here
+            print("stuck")
+            #if check in words and check not in tried:
+                # this could break
+                # eg, word a is wrong but the cipher word a creates causes word b to be correct
+                # peep - bread possiblity eg.
+
+                #tried.append(check)
+            if check not in words:
+                return True
+                # backtrack to begining?
+                # would 100% work but probs not most efficent
+
+def solve():
+    global version
+    global index
+
+    while True:
+        fill_board()
+        crackcode()
+        fill_board()
+        check_backtrack()
+
+        # if cipher doesnt work go back a verison, same version as before
+        if check_backtrack():
+            version -= 1
+            crackcode()
+        # if cipher does work go up an index
+        if not check_backtrack():
+            index += 1
+            crackcode()
 
 
 
+solve()
 
+# problem now is that if the code fucks up it cant change back and try again,
+# as the board isnt numbers anymore its letters
 
-
-
-# get word with the least amount of intergers and try that word
-
+# what if i somehow save every version of the board?
+# or at least the last3 versions?
+#
